b65f6adc21a566e34045428754ec4084fc9190bf
PepeHands <39165155+kaitoxdd@users.noreply.github.com>
2019-01-12 12:08:58 +0700

    Trang chủ cho admin

---
+++ traodoi.vn/admin/modules/index.php
@@ -0,0 +1,19 @@
+<?php require_once __DIR__. "/../layouts/header.php"; ?>
+                    <!-- Nội dung bên trong website-->
+                    <!-- Breadcrumbs-->
+                     <div id="content-wrapper">
+
+                        <div class="container-fluid">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item">
+                                    <a href="index.html">Dashboard</a>
+                                </li>
+                                <li class="breadcrumb-item active">Trang chủ Admin</li>
+                            </ol>
+                            <!-- Page Content -->
+                            <h1>Trang quản trị của website</h1>
+                            <hr>
+                            <p>Quản lý website của bạn tại đây.</p>
+                <!-- /.container-fluid -->
+                        </div>
+<?php require_once __DIR__. "/../layouts/footer.php"; ?>                
